78e379ea8d55d43f8ed28f504aa194f70998f804
Serdar Geldyev <serdarabad@gmail.com>
2025-08-02 18:24:51 +0300

    trello is working with empty boards

--- src/App.tsx
+++ src/App.tsx
@@ -6,71 +6,96 @@ import sourceData from "./data/sourceData.json";
 function App() {
   const [boards, setBoards] = useState<Board[]>(sourceData);
 
-  const [currentBd, setCurrentBd] = useState<Board | null>(null);
-  const [currentCard, setCurrentCard] = useState<Card | null>(null);
+  const [sourceBoard, setSourceBoard] = useState<Board | null>(null);
+  const [sourceCard, setSourceCard] = useState<Card | null>(null);
+
+  const [isHoveringOverCard, setIsHoveringOverCard] = useState<boolean>(false);
 
   function handleDragStart(
     _: DragEvent<HTMLDivElement>,
     bd: Board,
     card: Card
   ): void {
-    setCurrentBd(bd);
-    setCurrentCard(card);
+    setSourceBoard(bd); // save in state the board from where you take the card
+    setSourceCard(card); // save in state the card that you've picked
   }
 
   function handleDragOver(e: DragEvent<HTMLDivElement>): void {
-    e.preventDefault();
+    e.preventDefault(); // allows this element to be a valid target for dropping
     if (e.currentTarget.className === "card") {
+      // current target - element that we hover the card over
       e.currentTarget.style.boxShadow = "0 4px 3px grey";
+    }
+  }
+
+  function handleDragOverCard(e: DragEvent<HTMLDivElement>): void {
+    e.preventDefault(); // allows this element to be a valid target for dropping
+    if (e.currentTarget.className === "card") {
+      // current target - element that we hover the card over
+      e.currentTarget.style.boxShadow = "0 4px 3px grey";
+      setIsHoveringOverCard(true);
     }
   }
 
   function handleDragLeave(e: DragEvent<HTMLDivElement>): void {
     e.currentTarget.style.boxShadow = "none";
+    setIsHoveringOverCard(false);
   }
 
   function handleDrop(
     e: DragEvent<HTMLDivElement>,
-    bd: Board,
-    card: Card
+    dropBoard: Board, // board that we drop card to
+    dropCard: Card // card on which we drop card
   ): void {
     e.preventDefault();
-    if (!currentCard || !currentBd) return;
+    if (!sourceCard || !sourceBoard) return; // sort of type guarding, helping TS
 
-    const currentIndex = currentBd.items.indexOf(currentCard);
-    currentBd.items.splice(currentIndex, 1);
-    const dropIndex = bd.items.indexOf(card);
-    bd.items.splice(dropIndex + 1, 0, currentCard!);
+    const sourceIndex = sourceBoard.items.indexOf(sourceCard);
+    sourceBoard.items.splice(sourceIndex, 1); // remove card from the source board
+    const dropIndex = dropBoard.items.indexOf(dropCard);
+    dropBoard.items.splice(dropIndex + 1, 0, sourceCard); // add source card after drop card
     setBoards(
       boards.map((b) => {
-        if (b.id === bd.id) {
-          return bd;
+        // go over every board (b)
+        if (b.id === dropBoard.id) {
+          // if this is the board where we drop
+          return dropBoard; // replace it with changed drop board
         }
-        if (b.id === currentBd.id) {
-          return currentBd;
+        if (b.id === sourceBoard.id) {
+          // if this is the board where we pick from
+          return sourceBoard; // replace it with changed drop board
         }
-        return b;
+        return b; // if neither leave it alone
       })
     );
     e.currentTarget.style.boxShadow = "none";
   }
 
-  function handleDropCard(e: DragEvent<HTMLDivElement>, bd: Board): void {
-    if (!currentCard || !currentBd) return;
-    bd.items.push(currentCard);
-    const currentIndex = currentBd.items.indexOf(currentCard);
-    currentBd.items.splice(currentIndex, 1);
-    setBoards(
-      boards.map((b) => {
-        if (b.id === bd.id) {
-          return bd;
+  function handleDropCard(
+    e: DragEvent<HTMLDivElement>,
+    dropBoard: Board
+  ): void {
+    if (!sourceCard || !sourceBoard) return;
+    const sourceIndex = sourceBoard.items.indexOf(sourceCard);
+    sourceBoard.items.splice(sourceIndex, 1);
+
+    setBoards((boards) => {
+      return boards.map((b) => {
+        if (b.id === dropBoard.id) {
+          return { ...b, items: [...b.items, sourceCard] };
         }
-        if (b.id === currentBd.id) {
-          return currentBd;
+        if (b.id === sourceBoard.id) {
+          return {
+            ...b,
+            items: [
+              ...b.items.slice(0, sourceIndex),
+              ...b.items.slice(sourceIndex),
+            ],
+          };
         }
         return b;
-      })
-    );
+      });
+    });
     e.currentTarget.style.boxShadow = "none";
   }
 
@@ -86,18 +111,21 @@ function App() {
             className="board"
             key={bd.id}
             tabIndex={bd.id}
-            onDragOver={(e) => handleDragOver(e)}
-            onDrop={(e) => handleDropCard(e, bd)}
+            onDragOver={
+              isHoveringOverCard ? undefined : (e) => handleDragOver(e)
+            } // allows to drop things onto the board, board becomes a valid target
+            onDrop={
+              isHoveringOverCard ? undefined : (e) => handleDropCard(e, bd)
+            }
           >
             <div className="board-title">{bd.title}</div>
             {bd.items.map((card) => (
               <div
                 className="card"
-                tabIndex={card.id}
                 key={card.id}
                 draggable={true}
                 onDragStart={(e) => handleDragStart(e, bd, card)} // when start dragging
-                onDragOver={(e) => handleDragOver(e)} // when dragged element hovers over another one
+                onDragOver={(e) => handleDragOverCard(e)} // when dragged element hovers over another one
                 onDragLeave={(e) => handleDragLeave(e)} // when leaving hovering over another element
                 onDrop={(e) => handleDrop(e, bd, card)} // actually dropping the dragged element
                 onDragEnd={(e) => handleDragEnd(e)} // clean up fn when finished dragging, successfully or not
